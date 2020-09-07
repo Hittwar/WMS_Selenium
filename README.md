dde377998e919b80f75d3550264bd3240c642e48
hittwar <hittwar@hotmail.com>
2020-09-07 11:13:06 -0500

    readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# WMS_Selenium
-WMS by BY testing with Selenium
+# WMS - Selenium Web Driver
+Login in WMS with selenium web driver, and access to inventory menu
+
+# Steps to run server
+
+1. Create your virtual environment for example: virtualenv venv -p python3
+2. Activate virtual environment:
+a.  windows: .\venv\Scripts\activate
+3. Clone this project
+4. Intall requirements: pip install -r requirements.txt
+5. run: python login_wms.py
--- login_wms.py
+++ login_wms.py
@@ -1,11 +1,14 @@
+#Libraries import
+
 import unittest
 from pyunitreport import HTMLTestRunner
 from selenium import webdriver
 import time
 
+# Testing Set
 class HomePageTest(unittest.TestCase):
 
-    # setup 
+    # setup driver
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path= r'./chromedriver.exe')
         driver = self.driver
@@ -14,22 +17,28 @@ class HomePageTest(unittest.TestCase):
     
     def test_wms_local(self):
         driver = self.driver
+        #Web Service to test
         driver.get("http://localhost:8060/rp/login")
         time.sleep(2)
 
+        #field ID for user
         loginUserName = driver.find_element_by_id('loginUserName')
         loginUserName.clear()
         loginUserName.send_keys('super')
 
+        #field ID for Password
         loginPassword = driver.find_element_by_id('loginPassword')
         loginPassword.clear()
         loginPassword.send_keys('SUPER')
 
+        #field ID for button
         loginButton = driver.find_element_by_id('loginButton')
         loginButton.click()
         time.sleep(20)
 
+        #element ID for Menu 
         driver.find_element_by_id('ext-comp-1017-btnIconEl').click()
+        #element ID with name Inventory
         driver.find_element_by_link_text('INVENTORY').click()
         time.sleep(10)
     
